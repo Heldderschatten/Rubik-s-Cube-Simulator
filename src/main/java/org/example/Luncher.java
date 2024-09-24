9701ace268b4f2de7b5cc84becf9fc4689c6ad74
Sebastian Wald <97028479+Heldderschatten@users.noreply.github.com>
2024-09-24 14:56:03 +0200

    Try to create a Jar file

---
+++ src/main/java/org/example/Luncher.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javafx.application.Application;
+import org.example.rubikscubeb6.HelloApplication;
+
+public class Luncher {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
